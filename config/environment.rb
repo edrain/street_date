a647373039ec2b4437e5e6b696ed2c05bb69ef98
edrain <edrain@gmail.com>
2013-03-22 21:47:09 -0700

    environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
+  ENV['GEM_PATH'] = '/home/edrain/.gems' + ':/usr/lib/ruby/gems/1.8'
+end
+
 # Initialize the rails application
 StreetDate::Application.initialize!
+
