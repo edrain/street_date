ec70f9a9b6ef9e3507e4664ac24213c2ece2e5a1
edrain <edrain@gmail.com>
2013-03-23 12:23:14 -0700

    env

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 #if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
-  ENV['GEM_PATH'] = '/home/edrain/.gems' + ':/usr/lib/ruby/gems/1.8'
-  ENV['GEM_ENV'] = '/home/edrain/.gems' + ':/usr/lib/ruby/gems/1.8'
+  #ENV['GEM_PATH'] = '/home/edrain/.gems' + ':/usr/lib/ruby/gems/1.8'
+  #ENV['GEM_ENV'] = '/home/edrain/.gems' + ':/usr/lib/ruby/gems/1.8'
 #end
 
 #require File.join(File.dirname(__FILE__), 'boot')
