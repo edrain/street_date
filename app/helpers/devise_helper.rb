a3ea1820b15cc0e4f37ca48cf2a33927ecef61e3
edrain <edrain@gmail.com>
2013-03-23 12:30:25 -0700

    devise_helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,8 +5,10 @@ module DeviseHelper
 #    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t('errors.messages.not_saved',
-      count: resource.errors.count,
-      resource: resource.class.model_name.human.downcase)
+      #count: resource.errors.count,
+      #resource: resource.class.model_name.human.downcase)
+      :count => resource.errors.count,
+      :resource => resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div class="alert alert-notice alert-block form_label">
