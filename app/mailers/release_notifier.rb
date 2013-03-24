06d1101cc62eb56421fbff6dd955221299f059ff
edrain <edrain@gmail.com>
2013-03-23 17:27:11 -0700

    release notifier

--- app/mailers/release_notifier.rb
+++ app/mailers/release_notifier.rb
@@ -1,9 +1,9 @@
 class ReleaseNotifier < ActionMailer::Base
-  default from: 'Street Date <no-reply@streetdate.info>'
+  default :from => 'Street Date <no-reply@streetdate.info>'
 
   def notify(user)
     @user = user
-    mail to: user[0].email, subject: "You have new albums out today!"
+    mail :to => user[0].email, :subject => "You have new albums out today!"
     #mail(to: user[0].email, subject: "You have new albums out today!") do |format|
     #  format.html { render :layout => 'test_layout' }
     #  format.text 
@@ -12,6 +12,6 @@ class ReleaseNotifier < ActionMailer::Base
 
   def admin_notify(today)
     @today = today
-    mail to: 'eli@streetdate.info', subject: "Release notifications sent"    
+    mail :to => 'eli@streetdate.info', :subject => "Release notifications sent"    
   end  
 end
