c674271f56821d6bede8a13040b4ba4f42ca4417
edrain <edrain@gmail.com>
2013-03-26 18:58:16 -0700

    production mailer only

--- config/environment.rb
+++ config/environment.rb
@@ -10,21 +10,6 @@ require File.expand_path('../application', __FILE__)
 
 # configure action_mailer
 
-  # configure action_mailer
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address => 'mail.streetdate.info',
-  :port => 587,
-  :domain => 'streetdate.info',
-  :authentication => :login,
-  :user_name => 'edrain@streetdate.info',
-  :password => '007Spies'
-}
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_deliveries = true
-#config.action_mailer.default_charset = 'utf-8'
-
-
 # Initialize the rails application
 StreetDate::Application.initialize!
 
