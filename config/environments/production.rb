cfb47758ab712f5d9769fa9d861f41d343fdcf7f
edrain <edrain@gmail.com>
2013-03-26 19:23:52 -0700

    remove charset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ StreetDate::Application.configure do
     }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_charset = 'utf-8'
+  #config.action_mailer.default_charset = 'utf-8'
 
 
   # Enable threaded mode
