35d6b9061c959810fafc17696834cac8041d6c77
edrain <edrain@gmail.com>
2013-04-14 15:34:40 -0700

    testing notification problems

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ require File.expand_path('../application', __FILE__)
 #end
 
 #require File.join(File.dirname(__FILE__), 'boot')
+
+Base_iTunes_URL = "http://click.linksynergy.com/fs-bin/stat?id=UXyDgEy7SJ8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1="
   
 # Initialize the rails application
 StreetDate::Application.initialize!
