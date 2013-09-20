0c64cf55665995e0f546dec7a4890a936efdb46c
edrain <edrain@gmail.com>
2013-09-19 21:07:43 -0700

    Update URLs for iTunes affiliate program

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,11 @@ require File.expand_path('../application', __FILE__)
 
 #require File.join(File.dirname(__FILE__), 'boot')
 
-Base_iTunes_URL = "http://click.linksynergy.com/fs-bin/stat?id=UXyDgEy7SJ8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1="
+#Base_iTunes_URL = "http://click.linksynergy.com/fs-bin/stat?id=UXyDgEy7SJ8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1="
+#Base_iTunes_URL = "https://itunes.apple.com/us/album/"
+#Base_iTunes_Artist_URL = "https://itunes.apple.com/us/artist/"
+  
+Base_iTunes_Affiliate_Tag = "&at=11l79f"
   
 # Initialize the rails application
 StreetDate::Application.initialize!
